d52770c3f5ae6b7d67a824773be655341ab640f8
afwps <antoinefremiot@protonmail.com>
2020-08-10 18:49:18 +0200

    -

--- make_theater.sh
+++ make_theater.sh
@@ -14,3 +14,14 @@ echo "" >> README.md
 
 n=$(ls papes/ | wc -l)
 i=0
+
+for p in papes/*
+do
+    ((i++))
+    printf '%4d/%d: %s\n' "$i" "$n" "${p/papes\//}"
+
+    # creates thumbnails
+    convert -resize 200x "$p" "${p/papes/thumbnails}"
+    # makes entries in README
+    echo "[![${p/papes\//}](https://raw.githubusercontent.com/$username/$repo/blob/master/thumbnails${p/papes/})](https://raw.githubusercontent.com/$username/$repo/blob/master/$p)" >> README.md
+done
