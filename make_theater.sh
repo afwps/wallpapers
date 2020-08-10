02bc0a4c371a156dd8c11b2129492cb3e69d389c
afwps <antoinefremiot@protonmail.com>
2020-08-10 18:58:16 +0200

    Hotfixes

--- make_theater.sh
+++ make_theater.sh
@@ -1,15 +1,13 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 # set -e
 
-# deletes thumbnails folder if it already exists
 rm -rf thumbnails
-# creates a new one
 mkdir thumbnails
 
-user="afwps"
+username="afwps"
 repo="theatrum"
 
-cat HEADER.md > README.md
+cat readme_header.md > README.md
 echo "" >> README.md
 
 n=$(ls papes/ | wc -l)
@@ -23,5 +21,5 @@ do
     # creates thumbnails
     convert -resize 200x "$p" "${p/papes/thumbnails}"
     # makes entries in README
-    echo "[![${p/papes\//}](https://raw.githubusercontent.com/$username/$repo/blob/master/thumbnails${p/papes/})](https://raw.githubusercontent.com/$username/$repo/blob/master/$p)" >> README.md
+    echo "[![${p/papes\//}](https://raw.githubusercontent.com/$username/$repo/master/thumbnails${p/papes/})](https://raw.githubusercontent.com/$username/$repo/master/$p)" >> README.md
 done
